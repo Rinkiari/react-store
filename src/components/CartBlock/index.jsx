37805452ca3f2292007ef292ce9ad02d64e4127c
Rinkiari <st112446@student.spbu.ru>
2024-10-27 21:59:26 +0300

    xxxxxx

---
+++ src/components/CartBlock/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./CartBlock.module.scss";
+
+const CartBlock = () => {
+  console.log(styles);
+  return <p>smth</p>;
+};
+
+export default CartBlock;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import CartBlock from "../components/CartBlock/";
 
 const Cart = () => {
-  return <h1>CART</h1>;
+  return (
+    <div className="wrapchek">
+      <CartBlock />
+    </div>
+  );
 };
 
 export default Cart;
